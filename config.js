e3568300a95f3b3ae6db648d9109914e39ce23aa
HZ-33 <83805036+HZ-33@users.noreply.github.com>
2022-05-11 05:54:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 global.owner = [
   ['6289605393009'],
   ['0'],
-  ['6289605393009', 'Della', true]
+  ['6289605393009', 'immortal', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
